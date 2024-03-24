1cef9710772524ff676eea77ee68d91a41134239
Cassandra Lee <cassandratlee@gmail.com>
2024-03-23 23:06:29 -0230

    Finish calculation for inches

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 function calculatorFunction() {
-  let orgWidth = $("orgWidth").val();
+  let orgWidth = $("#orgWidth").val();
   let width = $("#width").val();
   let length = $("#length").val();
-  let costPerMeter = $("#cost").val();
-  let totalCost = parseFloat(width) + parseFloat(length);
-  document.getElementById("result").innerHTML = totalCost;
+  let costPerMeter = $("#costPM").val();
+
+  let percentageOfWidth = parseFloat(width) / parseFloat(orgWidth);
+  let lengthInM = parseFloat(length) / 39.5;
+  let priceFullPiece = lengthInM * parseFloat(costPerMeter);
+
+  let costOfPiece = (percentageOfWidth * priceFullPiece).toFixed(2);
+  document.getElementById("result").innerHTML = "$" + costOfPiece;
 }
